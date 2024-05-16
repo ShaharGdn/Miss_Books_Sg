8518c53f7d8def9d8d525b849e4cbf547b1cacec
ShaharGdn <shahargadon@gmail.com>
2024-05-16 16:35:20 +0300

    Home page styling

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,8 @@
 export function Home() {
     return <section className="home">
-        <h1>Home</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam sunt ducimus consectetur.</p>
+        <h1>Home Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non culpa laborum amet doloribus nam, est suscipit, blanditiis odit obcaecati facere ea optio velit veniam consequuntur ipsa quasi necessitatibus vero autem.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id et ipsa, suscipit voluptates, ea voluptate facere dicta cum tenetur illum neque ipsam! Repellat ipsa deserunt corporis officiis impedit totam facilis?</p>
+        <img src="assets/imgs/book-shop.png" alt="a book shop entrance" />
     </section>
 }
