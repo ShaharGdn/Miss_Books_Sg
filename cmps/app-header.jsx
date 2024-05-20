e8f463c724fa3888124df09eba3654709bca4d51
ShaharGdn <shahargadon@gmail.com>
2024-05-20 08:29:39 +0300

    Logo home navigation

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,9 +1,13 @@
 const { NavLink } = ReactRouterDOM
+const { useNavigate } = ReactRouter
+
 
 export function AppHeader() {
+    const navigate = useNavigate()
     return <header>
         <nav>
-            <h1>Miss Books</h1>
+            {/* <NavLink to="/book"><h1 className="logo">Miss Books</h1></NavLink> */}
+            <h1 className="logo" onClick={() => navigate('/book')}>Miss Books</h1>
             <section className="links">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/book">Books</NavLink>
